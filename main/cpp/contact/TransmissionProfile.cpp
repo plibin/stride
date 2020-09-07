1795440331aa8c566bb9e1a521348369b613c94c
Elise Kuylen <elise.kuylen@uantwerpen.be>
2020-09-07 13:07:10 +0200

    Distribution for infectious period length in theoretical description + small update on how parameters for transmission profile are read.

--- main/cpp/contact/TransmissionProfile.cpp
+++ main/cpp/contact/TransmissionProfile.cpp
@@ -31,39 +31,42 @@ using namespace boost::property_tree;
 void TransmissionProfile::Initialize(const ptree& configPt, const ptree& diseasePt, util::RnMan& rnMan)
 {
 		// Check wether to use r0 or transmission probability
-		boost::optional<double> r0AsInput = configPt.get_optional<double>("run.r0");
+		boost::optional<double> transmissionProbabilityAsInput = configPt.get_optional<double>("run.transmission_probability");
 
-		if (r0AsInput) {
-			// parse config file
-			const auto r0 = *r0AsInput;
-			const auto b0 = diseasePt.get<double>("disease.transmission.b0");
-			const auto b1 = diseasePt.get<double>("disease.transmission.b1");
-			const auto b2 = diseasePt.get<double>("disease.transmission.b2");
+		if (transmissionProbabilityAsInput) {
 
-			// if linear model is fitted to simulation data:
-			if(b2 == 0){
-			    	// Expected(R0) = (b0 + b1*transm_prob).
-			    	m_transmission_probability = (r0 - b0) / b1;
-
-			} else{
-				// if quadratic model is fitted to simulation data:
-			    	// Expected(R0) = (b0 + b1*transm_prob + b2*transm_prob^2).
-			    	// Find root
-				const auto a = b2;
-				const auto b = b1;
-				const auto c = b0 - r0;
-
-				// To obtain a real values (instead of complex)
-				if (r0 < (-(b * b) / (4 * a))) {
-					const double determ = (b * b) - 4 * a * c;
-					m_transmission_probability = (-b + sqrt(determ)) / (2 * a);
-				} else {
-					throw runtime_error("TransmissionProfile::Initialize> Illegal input values.");
-				}
-			}
-		} else {
 			// Use transmission probability as input parameter
-			m_transmission_probability = configPt.get<double>("run.transmission_probability");
+			m_transmission_probability = *transmissionProbabilityAsInput;
+
+		} else {
+			// Use R0 as input parameter
+			// parse config file
+				const auto r0 = configPt.get<double>("run.r0");
+				const auto b0 = diseasePt.get<double>("disease.transmission.b0");
+				const auto b1 = diseasePt.get<double>("disease.transmission.b1");
+				const auto b2 = diseasePt.get<double>("disease.transmission.b2");
+
+				// if linear model is fitted to simulation data:
+				if(b2 == 0){
+				    	// Expected(R0) = (b0 + b1*transm_prob).
+				    	m_transmission_probability = (r0 - b0) / b1;
+
+				} else{
+					// if quadratic model is fitted to simulation data:
+				    	// Expected(R0) = (b0 + b1*transm_prob + b2*transm_prob^2).
+				    	// Find root
+					const auto a = b2;
+					const auto b = b1;
+					const auto c = b0 - r0;
+
+					// To obtain a real values (instead of complex)
+					if (r0 < (-(b * b) / (4 * a))) {
+						const double determ = (b * b) - 4 * a * c;
+						m_transmission_probability = (-b + sqrt(determ)) / (2 * a);
+					} else {
+						throw runtime_error("TransmissionProfile::Initialize> Illegal input values.");
+					}
+				}
 		}
 
 		// Check whether transmission probability follows a distribution or is constant
--- main/python/estimate_transmission_probability.py
+++ main/python/estimate_transmission_probability.py
@@ -12,9 +12,9 @@ def get_contact_rates(pooltype, contact_rate_tree, maxAge):
         #contact_rates.append(0)
 
     # Get contact rates for the right pooltype from xml tree
-    '''max_participant_age = 0
+    max_participant_age = 0
     pooltype_contacts = contact_rate_tree.find(pooltype)
-    # FIXME This is for aggregated contacts by age (participants -> contacts -> contact -> age = all)
+    '''# FIXME This is for aggregated contacts by age (participants -> contacts -> contact -> age = all)
     for participant in pooltype_contacts.findall("participant"):
         participant_age = int(participant.find("age").text)
         if (participant_age > max_participant_age):
@@ -34,12 +34,12 @@ def get_contact_rates(pooltype, contact_rate_tree, maxAge):
             age2 = int(contact.find("age").text)
             contact_rate = float(contact.find("rate").text)
             contact_rates[age1][age2] = contact_rate
-    '''
-    # Fill in contact rates for ages > max_participant_age
+
+    '''# Fill in contact rates for ages > max_participant_age
     # Set contact_rate[age] = contact_rate[max_participant_age] if age > max_participant_age
     for age in range(max_participant_age + 1, maxAge + 1):
-        contact_rates[age] = contact_rates[max_participant_age]
-    '''
+        contact_rates[age] = contact_rates[max_participant_age]'''
+
     # Fill in contact rates for ages > max_participant_age
     # Set contact rage [age1, age2] = [max_participant_age, age2] if age2 <= max_participant_age
     # Else [age1, age2] = [max_participant_age, max_participant_age]
@@ -51,6 +51,7 @@ def get_contact_rates(pooltype, contact_rate_tree, maxAge):
             else:
                 contact_rates[age1][age2] = contact_rates[max_participant_age][max_participant_age]
                 contact_rates[age2][age1] = contact_rates[max_participant_age][max_participant_age]
+
     return contact_rates
 
 def add_to_pool(pools, pool_id, person_id, age):
@@ -61,34 +62,37 @@ def add_to_pool(pools, pool_id, person_id, age):
             pools[pool_id] = [(person_id, age)]
 
 def get_effective_contacts(person_id, age, pools, pooltype, pool_id, contact_rates,
-    infectious_period_length, transmission_probability):
+    infectious_period_lengths, transmission_probability):
     effective_contacts = 0
 
     # Iterate over pool members
     if pool_id > 0:
         pool_members = pools[pool_id]
         pool_size = len(pool_members)
-        for member in pool_members:
-            member_id = member[0]
-            member_age = member[1]
-            if member_id != person_id: # Check that this is not the same person
-                contact_rate = contact_rates[pooltype][age][member_age]
-                contact_probability = contact_rate / pool_size
-                '''# FIXME This is for aggregated contacts by age (participants -> contacts -> contact -> age = all)
-                contact_rate1 = contact_rates[pooltype][age]
-                contact_rate2 = contact_rates[pooltype][member_age]
-                contact_probability = min(contact_rate1, contact_rate2) / pool_size'''
-                # Households are assumed to be fully connected in Stride
-                if pooltype == "household":
-                    contact_probability = 0.999
-                    #contact_probability = 1
-                if contact_probability >= 1:
-                    print(contact_probability)
-                    #contact_probability = 0.999
-                effective_contacts += 1 - ((1 - (transmission_probability * contact_probability))**infectious_period_length)
+        for infectious_period in infectious_period_lengths:
+            for member in pool_members:
+                member_id = member[0]
+                member_age = member[1]
+                if member_id != person_id: # Check that this is not the same person
+                    contact_rate = contact_rates[pooltype][age][member_age]
+                    of_same_age_in_pool = sum([1 for x in pool_members if x[1] == member_age])
+                    contact_probability = contact_rate / of_same_age_in_pool
+                    # FIXME This is for aggregated contacts by age (participants -> contacts -> contact -> age = all)
+                    #contact_rate1 = contact_rates[pooltype][age]
+                    #contact_rate2 = contact_rates[pooltype][member_age]
+                    #contact_probability = min(contact_rate1, contact_rate2) / pool_size
+                    # Households are assumed to be fully connected in Stride
+                    if pooltype == "household":
+                        contact_probability = 0.999
+                        #contact_probability = 1
+                    if contact_probability >= 1:
+                        contact_probability = 0.999
+                    effective_contacts += 1 - ((1 - (transmission_probability * contact_probability))**infectious_period)
+    # Assuming uniform distribution of infectious period lengths
+    effective_contacts /= len(infectious_period_lengths)
     return effective_contacts
 
-def main(population_file, contact_matrix_file, infectious_period_length, transmission_probabilities):
+def main(population_file, contact_matrix_file, infectious_period_lengths, transmission_probabilities):
     maxAge = 111
 
     ############################################################################
@@ -153,24 +157,24 @@ def main(population_file, contact_matrix_file, infectious_period_length, transmi
     ############################################################################
     for tp in transmission_probabilities:
         all_effective_contacts = []
-        for person in population:
+        for person in random.choices(population, k=100):
             # Get # of effective contacts in each of the contact pools
             # to which this person belongs
             hh_effective_contacts = get_effective_contacts(person["id"], person["age"],
                                                             households, "household", person["household_id"],
-                                                            contact_rates, infectious_period_length, tp)
+                                                            contact_rates, infectious_period_lengths, tp)
             work_effective_contacts = get_effective_contacts(person["id"], person["age"],
                                                             workplaces, "workplace", person["work_id"],
-                                                            contact_rates, infectious_period_length, tp)
+                                                            contact_rates, infectious_period_lengths, tp)
             school_effective_contacts = get_effective_contacts(person["id"], person["age"],
                                                             schools, "school", person["school_id"],
-                                                            contact_rates, infectious_period_length, tp)
+                                                            contact_rates, infectious_period_lengths, tp)
             primary_community_effective_contacts = get_effective_contacts(person["id"], person["age"],
                                                             primary_communities, "primary_community", person["primary_community_id"],
-                                                            contact_rates, infectious_period_length, tp)
+                                                            contact_rates, infectious_period_lengths, tp)
             secondary_community_effective_contacts = get_effective_contacts(person["id"], person["age"],
                                                             secondary_communities, "secondary_community", person["secondary_community_id"],
-                                                            contact_rates, infectious_period_length, tp)
+                                                            contact_rates, infectious_period_lengths, tp)
 
             total_effective_contacts = hh_effective_contacts + work_effective_contacts + school_effective_contacts + primary_community_effective_contacts + secondary_community_effective_contacts
             all_effective_contacts.append(total_effective_contacts)
@@ -182,7 +186,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("population_file", type=str)
     parser.add_argument("contact_matrix_file", type=str)
-    parser.add_argument("--infectious_period_length", type=int, default=7, help="Mean length of infectious period (in days)")
+    parser.add_argument("--infectious_period_lengths", type=int, nargs="+", default=[6,7,8,9], help="Mean length of infectious period (in days)")
     parser.add_argument("--transmission_probabilities", type=float, nargs="+", default=[0.02, 0.04, 0.06, 0.08, 0.10])
     args = parser.parse_args()
-    main(args.population_file, args.contact_matrix_file, args.infectious_period_length, args.transmission_probabilities)
+    main(args.population_file, args.contact_matrix_file, args.infectious_period_lengths, args.transmission_probabilities)
